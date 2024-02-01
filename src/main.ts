89a52a152d107cb9b8ab6b212b9a0d623dd9acdd
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-02-01 18:49:08 +0300

    Commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+/// <reference types="@angular/localize" />
+
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app/app.component';
