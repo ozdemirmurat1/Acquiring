1719253fb1b3affa7b07518e1d0e266e4b3c65f6
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-02-07 15:30:43 +0300

    Commit

--- src/app/ui/components/auth/services/auth.service.ts
+++ src/app/ui/components/auth/services/auth.service.ts
@@ -5,39 +5,65 @@ import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/
 import { LoginResponseModel } from '../models/Login-Response.model';
 import { RequestLoginModel } from '../models/Request-Login.model';
 import { CryptoService } from 'src/app/common/directives/services/crypto.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Errors } from 'src/app/common/models/error-model';
+import { AlertifyService, MessageType, Position } from 'src/app/common/directives/services/alertify.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService,private _crypto:CryptoService) { }
+  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService,private _crypto:CryptoService,private alertifyService: AlertifyService) { }
 
-  async login(model:RequestLoginModel, callBackFunction?: () => void): Promise<any> {
-    debugger;
-    const observable: Observable<any | LoginResponseModel> = this.httpClientService.post<any | LoginResponseModel>({
-      controller: "auth",
-      action: "login"
-    }, { Email:model.Email,Password:model.Password})
+  async login(model:RequestLoginModel, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<any> {
 
-    const loginResponseModel: LoginResponseModel = await firstValueFrom(observable) as LoginResponseModel;
-    let cryptoAccessTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.accessToken.token))
-    let cryptoRefreshTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.refreshTokenDto.token))
-    
-    if (loginResponseModel) {
-      // BURADAKİ ACCESS VE REFRESHTOKEN LARI DEĞER İLE TAŞI İDENETİYCHECK TE KULLAN
-      localStorage.setItem("accessToken", cryptoAccessTokenValue);
-      localStorage.setItem("refreshToken", cryptoRefreshTokenValue);
-      _isAuthenticatedAccessTokenExpire=loginResponseModel.accessToken.expiration;
-      _isAuthenticatedRefreshTokenExpire=loginResponseModel.refreshTokenDto.expiration;
+    try {
+      const observable: Observable<any | LoginResponseModel> = this.httpClientService.post<any | LoginResponseModel>({
+        controller: "auth",
+        action: "login"
+      }, { Email:model.Email,Password:model.Password})
+  
+      const loginResponseModel: LoginResponseModel = await firstValueFrom(observable) as LoginResponseModel;
+      let cryptoAccessTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.accessToken.token))
+      let cryptoRefreshTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.refreshTokenDto.token))
+  
+      if (loginResponseModel) {
+        // BURADAKİ ACCESS VE REFRESHTOKEN LARI DEĞER İLE TAŞI İDENETİYCHECK TE KULLAN
+        localStorage.setItem("accessToken", cryptoAccessTokenValue);
+        localStorage.setItem("refreshToken", cryptoRefreshTokenValue);
+        _isAuthenticatedAccessTokenExpire=loginResponseModel.accessToken.expiration;
+        _isAuthenticatedRefreshTokenExpire=loginResponseModel.refreshTokenDto.expiration;
+  
+        this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
+          messageType: ToastrMessageType.Success,
+          position: ToastrPosition.TopRight
+        })
+      }
+    } catch (error: any) {
 
-      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
-        messageType: ToastrMessageType.Success,
-        position: ToastrPosition.TopRight
-      })
+      error:HttpErrorResponse;
+      let customError: Errors;
+
+      customError = error.error;
+      debugger;
+
+      // const _error: Array<{ Property: string, Errors: Array<string> }> = customError.Errors;
+      // let message = "";
+      // _error.forEach((v, index) => {
+      //   v.Errors.forEach((_v, _index) => {
+      //     message += `${_v}<br>`;
+      //   });
+      // });
+
+      this.alertifyService.message(customError.detail,
+        {
+          dismissOthers: true,
+          messageType: MessageType.Error,
+          position: Position.TopRight
+        });
     }
-
-    callBackFunction();
+   
   }
 }
 
