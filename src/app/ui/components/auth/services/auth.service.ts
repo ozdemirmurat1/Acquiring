a7288f5c060349207e8b4376f0118fb67db0d07e
Murat Ozdemir <murat.ozdemir@payexpert.com.tr>
2024-02-07 21:09:15 +0300

    Commit

--- src/app/ui/components/auth/services/auth.service.ts
+++ src/app/ui/components/auth/services/auth.service.ts
@@ -14,51 +14,39 @@ import { AlertifyService, MessageType, Position } from 'src/app/common/directive
 })
 export class AuthService {
 
-  constructor(private httpClientService:HttpClientService,private toastrService:CustomToastrService,private _crypto:CryptoService,private alertifyService: AlertifyService) { }
+  constructor(private httpClientService: HttpClientService, private toastrService: CustomToastrService, private _crypto: CryptoService, private alertifyService: AlertifyService) { }
 
-  async login(model:RequestLoginModel, successCallBack?: () => void, errorCallBack?: (errorMessage: string) => void): Promise<any> {
+  async login(model: RequestLoginModel, callBackFunction?: () => void): Promise<any> {
 
-    try {
-      const observable: Observable<any | LoginResponseModel> = this.httpClientService.post<any | LoginResponseModel>({
-        controller: "auth",
-        action: "login"
-      }, { Email:model.Email,Password:model.Password})
-  
-      const loginResponseModel: LoginResponseModel = await firstValueFrom(observable) as LoginResponseModel;
-      let cryptoAccessTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.accessToken.token))
-      let cryptoRefreshTokenValue=this._crypto.encrypto(JSON.stringify(loginResponseModel.refreshTokenDto.token))
-  
-      if (loginResponseModel) {
-        // BURADAKİ ACCESS VE REFRESHTOKEN LARI DEĞER İLE TAŞI İDENETİYCHECK TE KULLAN
-        localStorage.setItem("accessToken", cryptoAccessTokenValue);
-        localStorage.setItem("refreshToken", cryptoRefreshTokenValue);
-        _isAuthenticatedAccessTokenExpire=loginResponseModel.accessToken.expiration;
-        _isAuthenticatedRefreshTokenExpire=loginResponseModel.refreshTokenDto.expiration;
-  
-        this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
-          messageType: ToastrMessageType.Success,
-          position: ToastrPosition.TopRight
-        })
-      }
-    } catch (error: any) {
 
-      error:HttpErrorResponse;
-      let customError: Errors;
+    const observable: Observable<any | LoginResponseModel> = this.httpClientService.post<any | LoginResponseModel>({
+      controller: "auth",
+      action: "login"
+    }, { Email: model.Email, Password: model.Password })
 
-      customError = error.error;
+    const loginResponseModel: LoginResponseModel = await firstValueFrom(observable) as LoginResponseModel;
+    let cryptoAccessTokenValue = this._crypto.encrypto(JSON.stringify(loginResponseModel.accessToken.token))
+    let cryptoRefreshTokenValue = this._crypto.encrypto(JSON.stringify(loginResponseModel.refreshTokenDto.token))
 
-      this.alertifyService.message(customError.detail,
-        {
-          dismissOthers: true,
-          messageType: MessageType.Error,
-          position: Position.TopRight
-        });
+    if (loginResponseModel) {
+      // BURADAKİ ACCESS VE REFRESHTOKEN LARI DEĞER İLE TAŞI İDENETİYCHECK TE KULLAN
+      localStorage.setItem("accessToken", cryptoAccessTokenValue);
+      localStorage.setItem("refreshToken", cryptoRefreshTokenValue);
+      _isAuthenticatedAccessTokenExpire = loginResponseModel.accessToken.expiration;
+      _isAuthenticatedRefreshTokenExpire = loginResponseModel.refreshTokenDto.expiration;
+
+      this.toastrService.message("Kullanıcı girişi başarıyla sağlanmıştır.", "Giriş Başarılı", {
+        messageType: ToastrMessageType.Success,
+        position: ToastrPosition.TopRight
+      })
     }
-   
+
+    callBackFunction();
+
   }
 }
 
 export let _isAuthenticatedAccessTokenExpire: string;
 export let _isAuthenticatedRefreshTokenExpire: string;
-export let _accessToken:string;
-export let _refreshToken:string;
+export let _accessToken: string;
+export let _refreshToken: string;
